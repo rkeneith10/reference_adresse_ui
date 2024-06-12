40731c98843c8f7346b7f051a2220b3363bf9eb6
Keneith-Salnave Romain <rkeneith10@yahoo.com>
2024-06-12 16:09:30 -0400

    adding suspense

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import Chart from "@/components/barchart";
 import RootLayout from "@/components/rootLayout";
-import { CountryAttributes } from "../api/models/paysModel";
-//import countries from "@/data/pays";
+import { Spinner } from "@nextui-org/react";
 import axios from "axios";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaRegFlag, FaTreeCity } from "react-icons/fa6";
+import { CountryAttributes } from "../api/models/paysModel";
 
 const Home: React.FC = () => {
   const [countries, setCountries] = useState<CountryAttributes[]>([]);
+  const [loading, setLoading] = useState<Boolean>(true);
+
   useEffect(() => {
     document.title = "Tableau de bord";
 
@@ -17,56 +19,67 @@ const Home: React.FC = () => {
       try {
         const response = await axios.get("/api/paysCtrl");
         setCountries(response.data.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching products:", error);
       }
     };
     fetchData();
   }, []);
+
   return (
     <RootLayout isAuthenticated={true}>
-      <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex flex-wrap justify-center md:justify-between">
-        <div className="w-full md:w-1/3 p-4">
-          <div className="bg-white shadow-md rounded-md p-6 text-center">
-            <div className="flex justify-center items-center mb-4">
-              <h2 className="text-lg font-semibold text-blue-600">Pays</h2>
-              <span className="ml-2">
-                <FaRegFlag size={20} className="text-blue-500" />
-              </span>
-            </div>
-            <p className="font-bold text-lg text-blue-500">
-              {countries.length}
-            </p>
+      <>
+        {loading ? (
+          <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+            <Spinner size="lg" color="primary" />
+            <div className="loader">Chargement en cours...</div>
           </div>
-        </div>
-        <div className="w-full md:w-1/3 p-4">
-          <div className="bg-white shadow-md rounded-md p-6 text-center">
-            <div className="flex justify-center items-center mb-4">
-              <h2 className="text-lg font-semibold text-blue-600">Villes</h2>
-              <span className="ml-2">
-                <FaTreeCity size={20} className="text-blue-500" />
-              </span>
+        ) : (
+          <>
+            <div className="flex flex-wrap justify-center md:justify-between">
+              <div className="w-full md:w-1/3 p-4">
+                <div className="bg-white shadow-md rounded-md p-6 text-center">
+                  <div className="flex justify-center items-center mb-4">
+                    <h2 className="text-lg font-semibold text-blue-600">Pays</h2>
+                    <span className="ml-2">
+                      <FaRegFlag size={20} className="text-blue-500" />
+                    </span>
+                  </div>
+                  <p className="font-bold text-lg text-blue-500">
+                    {countries.length}
+                  </p>
+                </div>
+              </div>
+              <div className="w-full md:w-1/3 p-4">
+                <div className="bg-white shadow-md rounded-md p-6 text-center">
+                  <div className="flex justify-center items-center mb-4">
+                    <h2 className="text-lg font-semibold text-blue-600">Villes</h2>
+                    <span className="ml-2">
+                      <FaTreeCity size={20} className="text-blue-500" />
+                    </span>
+                  </div>
+                  <p className="font-bold text-lg text-blue-500">110</p>
+                </div>
+              </div>
+              <div className="w-full md:w-1/3 p-4">
+                <div className="bg-white shadow-md rounded-md p-6 text-center">
+                  <div className="flex justify-center items-center mb-4">
+                    <h2 className="text-lg font-semibold text-blue-600">Adresses</h2>
+                    <span className="ml-2">
+                      <FaMapMarkerAlt size={20} className="text-blue-500" />
+                    </span>
+                  </div>
+                  <p className="font-bold text-lg text-blue-500">127</p>
+                </div>
+              </div>
             </div>
-            <p className="font-bold text-lg text-blue-500">110</p>
-          </div>
-        </div>
-        <div className="w-full md:w-1/3 p-4">
-          <div className="bg-white shadow-md rounded-md p-6 text-center">
-            <div className="flex justify-center items-center mb-4">
-              <h2 className="text-lg font-semibold text-blue-600">Adresses</h2>
-              <span className="ml-2">
-                <FaMapMarkerAlt size={20} className="text-blue-500" />
-              </span>
+            <div className="">
+              <Chart />
             </div>
-            <p className="font-bold text-lg text-blue-500">127</p>
-          </div>
-        </div>
-      </div>
-      <div className="">
-        <Chart />
-      </div>
-      </Suspense>
+          </>
+        )}
+      </>
     </RootLayout>
   );
 };
