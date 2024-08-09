d45ebff2f491b531b9c32996446f330c94803d1f
Keneith-Salnave Romain <rkeneith10@yahoo.com>
2024-08-09 15:04:21 -0400

    table ville added

--- app/actions/actionSectionCommunale.ts
+++ app/actions/actionSectionCommunale.ts
@@ -3,12 +3,12 @@
 import SectionCommunale from "../api/models/sectionCommunalModel";
 
 export async function updateSectionCommunale(
-  id_sectioncommune: number,
+  id_sectioncommunale: number,
   libelle: string,
   id_ville: number
 
 ) {
-  const com = await SectionCommunale.findOne({ where: { id_sectioncommune } });
+  const com = await SectionCommunale.findOne({ where: { id_sectioncommunale } });
   if (!com) {
     throw new Error("Section Communale not found");
   }
