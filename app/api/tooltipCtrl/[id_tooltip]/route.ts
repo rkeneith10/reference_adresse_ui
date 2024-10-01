e0dd71eb55ef01b6c6c99c2af7a1112706833137
Keneith-Salnave Romain <rkeneith10@yahoo.com>
2024-10-01 15:05:20 -0400

    delete and update tooltip

--- app/api/tooltipCtrl/[id_tooltip]/route.ts
+++ app/api/tooltipCtrl/[id_tooltip]/route.ts
@@ -87,6 +87,56 @@ export async function DELETE(
   }
 }
 
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id_tooltip: number } }
+) {
+  try {
+    const { id_tooltip } = params;
+
+    if (!id_tooltip) {
+      return NextResponse.json({ error: "Invalid Id" }, { status: 400 });
+    }
+
+    // Parse the request body to get the updated data
+    const updatedData = await req.json();
+
+    // Find the existing tooltip
+    const tooltip = await Tooltip.findOne({ where: { id_tooltip } });
+    if (!tooltip) {
+      return NextResponse.json(
+        { message: "Le tooltip n'existe pas." },
+        { status: 404 }
+      );
+    }
+
+    // Update the tooltip with the new data
+    await Tooltip.update(updatedData, { where: { id_tooltip } });
+
+    const response = NextResponse.json(
+      { message: "Le message d'aide a été mis à jour avec succès." },
+      { status: 200 }
+    );
+
+    // Ajout des en-têtes CORS
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    return response;
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        error: "Erreur lors de la mise à jour du tooltip",
+        details: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
+
 // Gérer les requêtes OPTIONS pour CORS (préflight requests)
 export function OPTIONS() {
   const response = NextResponse.json({});
