c2e672a1e0eb0758affe850e7b8b6d5dff8d219b
Keneith-Salnave Romain <rkeneith10@yahoo.com>
2025-03-18 12:53:33 -0400

    adresse change

--- app/api/adresseCtrl/route.ts
+++ app/api/adresseCtrl/route.ts
@@ -3,22 +3,37 @@ import { Op } from "sequelize";
 import Adresse from "../models/adresseModel";
 import Commune from "../models/communeModel";
 import Departement from "../models/departementModel";
-import Pays from "../models/paysModel";
+import { default as Country, default as Pays } from "../models/paysModel";
 
 
 export async function GET(req: NextRequest) {
   try {
+    const adresses = await Adresse.findAll({
+      include: [
+        {
+          model: Commune,
+          attributes: ["libelle_commune", "id_commune"],
+          include: [
+            {
+              model: Departement,
+              attributes: ["libelle_departement", "id_departement"],
+              include: [
+                {
+                  model: Country,
+                  attributes: ["libelle_pays", "id_pays"],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      order: [["id_adresses", "DESC"]],
+    });
 
-    const adr = await Adresse.findAll({});
-    if (adr) {
-      const response = NextResponse.json({ data: adr }, { status: 200 });
-
-      return response;
-    }
+    return NextResponse.json({ data: adresses }, { status: 200 });
   } catch (error: any) {
     console.error(error);
-    const response = NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-    return response;
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
 
@@ -41,65 +56,61 @@ export async function POST(req: NextRequest) {
 
 
 
-      const commune = await Commune.findOne({ where: { id_commune } });
-      if (!commune) {
-        const response = NextResponse.json({ message: "Commune not found." }, { status: 404 });
-        return response;
-      }
-
-      const departement = await Departement.findOne({ where: { id_departement: commune.id_departement } });
-      if (!departement) {
-        const response = NextResponse.json({ message: "Departement not found." }, { status: 404 });
-        return response;
-      }
-
-      const pays = await Pays.findOne({ where: { id_pays: departement.id_pays } });
-      if (!pays) {
-        const response = NextResponse.json({ message: "Pays not found." }, { status: 404 });
-        return response;
-      }
-
-      cle_unicite_base = `${pays.code_pays}${departement.code_departement}${code_postal}${numero_rue || 'X'}${libelle_adresse
-        .charAt(0)
-        .toUpperCase()}${libelle_adresse.replace(/[aeiouAEIOU\s]/g, '').toUpperCase()}`;
-
-      // Trouver le plus grand numéro de séquence pour des clés similaires
-      const similarKeys = await Adresse.findAll({
-        where: {
-          cle_unicite: {
-            [Op.like]: `${cle_unicite_base}%`,
-          },
-        },
-      });
-
-      let sequence = '01';
-      if (similarKeys.length > 0) {
-        const highestSequence = Math.max(...similarKeys.map(key => parseInt(key.cle_unicite.slice(-2))));
-        sequence = (highestSequence + 1).toString().padStart(2, '0');
-      }
-
-      cle_unicite = `${cle_unicite_base}${sequence}`;
-
-
-      const adresse = await Adresse.create({
-        numero_rue,
-        libelle_adresse,
-        statut,
-        id_commune,
-        section_communale,
-        code_postal,
-        cle_unicite,
-        from,
-      });
-
-      const response = NextResponse.json(
-        { message: "Adresse created successfully", adresse },
-        { status: 201 }
-      );
+    const commune = await Commune.findOne({ where: { id_commune } });
+    if (!commune) {
+      const response = NextResponse.json({ message: "Commune not found." }, { status: 404 });
       return response;
-    
+    }
+
+    const departement = await Departement.findOne({ where: { id_departement: commune.id_departement } });
+    if (!departement) {
+      const response = NextResponse.json({ message: "Departement not found." }, { status: 404 });
+      return response;
+    }
+
+    const pays = await Pays.findOne({ where: { id_pays: departement.id_pays } });
+    if (!pays) {
+      const response = NextResponse.json({ message: "Pays not found." }, { status: 404 });
+      return response;
+    }
+
+    cle_unicite_base = `${pays.code_pays}${departement.code_departement}${code_postal}${numero_rue || 'X'}${libelle_adresse
+      .charAt(0)
+      .toUpperCase()}${libelle_adresse.replace(/[aeiouAEIOU\s]/g, '').toUpperCase()}`;
+
+    // Trouver le plus grand numéro de séquence pour des clés similaires
+    const similarKeys = await Adresse.findAll({
+      where: {
+        cle_unicite: {
+          [Op.like]: `${cle_unicite_base}%`,
+        },
+      },
+    });
+
+    let sequence = '01';
+    if (similarKeys.length > 0) {
+      const highestSequence = Math.max(...similarKeys.map(key => parseInt(key.cle_unicite.slice(-2))));
+      sequence = (highestSequence + 1).toString().padStart(2, '0');
+    }
+
+    cle_unicite = `${cle_unicite_base}${sequence}`;
 
 
+    const adresse = await Adresse.create({
+      numero_rue,
+      libelle_adresse,
+      statut,
+      id_commune,
+      section_communale,
+      code_postal,
+      cle_unicite,
+      from,
+    });
+
+    const response = NextResponse.json(
+      { message: "Adresse created successfully", adresse },
+      { status: 201 }
+    );
     return response;
   } catch (error: any) {
     console.error(error);
@@ -107,7 +118,7 @@ export async function POST(req: NextRequest) {
     return response;
   }
 
-  
+
 }
 
 
